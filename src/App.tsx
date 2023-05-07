16d1051ca5a01765ba5b152ee6cbbe95cf88c6b3
yokkepp <yoseri430@gmail.com>
2023-05-07 11:48:03 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+//enum
+
+function App() {
+	return (
+		<>
+			<input type='file' />
+		</>
+	);
+}
+
+export default App;
